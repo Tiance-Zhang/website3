92bcb7d2298c3b7d538866785db7409ef6649c57
Glitch (starter-react-template) <none>
2020-02-23 16:55:42 +0000

    🧀🍳 Checkpoint ./src/components/square.js:10218203/281 ./src/components/board.js:10218203/828 ./src/components/game.js:10218203/432 ./src/App.css:10218203/492 ./src/App.js:10218203/1011 ./src/index.css:10218203/489 ./src/components/square.css:10218203/333 ./src/components/game.css:10218203/86 ./src/components/board.css:10218203/105 ./src/index.js:10218203/13 ./public/index.html:10218203/8543

---
+++ src/components/square.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './square.css';
+
+class Square extends Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+export { Square }
