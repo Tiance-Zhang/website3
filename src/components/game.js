e00ad1276db24c548a85dca6f00fe7fd69a707b5
Glitch (starter-react-template) <none>
2020-02-26 04:22:35 +0000

    👷🌯 Checkpoint ./src/components/game.js:10221711/4787 ./src/index.js:10221711/1176

--- src/components/game.js
+++ src/components/game.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Board } from './board';
 import './game.css';
-import {calculateWinner} from '/index';
 
 class Game extends Component {
   constructor(props){
@@ -13,6 +12,8 @@ class Game extends Component {
       xIsNext:true,
     };
   }
+  
+
   
   handleClick(i) {
     const history = this.state.history;
@@ -60,7 +61,25 @@ class Game extends Component {
   }
 }
 
-
+ function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 
 
 export { Game }
--- src/index.js
+++ src/index.js
@@ -6,25 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-export function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+
 
 
 // If you want your app to work offline and load faster, you can change
