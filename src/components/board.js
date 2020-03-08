ed6be2d856f97fb8f01430ad1961707daa780c10
Glitch (starter-react-template) <none>
2020-03-08 04:47:12 +0000

    🌇💃 Checkpoint ./src/components/square.css:10221707/997 ./src/components/board.js:10221707/6385

--- src/components/board.js
+++ src/components/board.js
@@ -1,96 +1,96 @@
-import React, { Component } from 'react';
-import { Square } from './square';
-import './board.css';
+import React, { Component } from "react";
+import { Square } from "./square";
+import "./board.css";
 
 class Board extends Component {
-
   rowNum = 9;
-  colNum =  9;
+  colNum = 9;
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {stones: Array(this.rowNum * this.colNum).fill(null),
-                  stonesBf: Array(this.rowNum * this.colNum).fill(null),
-                  isBlack: true};
+    this.state = {
+      stones: Array(this.rowNum * this.colNum).fill(null),
+      stonesBf: Array(this.rowNum * this.colNum).fill(null),
+      isBlack: true
+    };
   }
 
   handleClick(i) {
     const bfStones = this.state.stones.slice(); // to avoid same-pointer thing
     const crtStones = this.state.stones.slice(); // make a copy. I didn't use this.
-    if (crtStones[i] == null){
-      this.setState({stonesBf: bfStones});
-      crtStones[i] = this.state.isBlack? '●' : '○';
-      this.setState({stones: crtStones, isBlack: !this.state.isBlack});
+    if (crtStones[i] == null) {
+      this.setState({ stonesBf: bfStones });
+      crtStones[i] = this.state.isBlack ? "●" : "○";
+      this.setState({ stones: crtStones, isBlack: !this.state.isBlack });
       // console.log(stones);
       this.props.onStoneNumUpdated(crtStones.filter(s => s != null).length);
       //props can be anything
-    }
-    else{    //remove an existing stone
-      this.setState({stonesBf: bfStones});
+    } else {
+      //remove an existing stone
+      this.setState({ stonesBf: bfStones });
       crtStones[i] = null;
-      this.setState({stones: crtStones});
+      this.setState({ stones: crtStones });
       // console.log(crtStones);
       this.props.onStoneNumUpdated(crtStones.filter(s => s != null).length);
     }
-
   }
 
   renderSquare(i) {
-    return <Square key={"square-" + i} value={this.state.stones[i]} onClick={() => this.handleClick(i)} />;
+    return (
+      <Square
+        key={"square-" + i}
+        value={this.state.stones[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
-  undo(){
-    this.setState({stones: this.state.stonesBf, isBlack: !this.state.isBlack});
+  undo() {
+    this.setState({
+      stones: this.state.stonesBf,
+      isBlack: !this.state.isBlack
+    });
   }
-  pass(){
-    this.setState({isBlack: !this.state.isBlack});
+  pass() {
+    this.setState({ isBlack: !this.state.isBlack });
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.isBlack? "Black" : "White");
-
+    const status = "Next player: " + (this.state.isBlack ? "Black" : "White");
 
     //const means you cannot change what it points to.
     //const row = []; then row =[1, 2, 3] is invalid!
     //but row.push() is OK.
     const showBoard = [];
 
-    for (let ri = 0; ri < this.rowNum; ri++){
+    for (let ri = 0; ri < this.rowNum; ri++) {
       const eachRow = [];
-      for (let cj = 0; cj < this.colNum; cj++){
+      for (let cj = 0; cj < this.colNum; cj++) {
         eachRow.push(this.renderSquare(ri * this.rowNum + cj));
       }
-      showBoard.push(<div key={"row" + ri} className="board-row">{eachRow}</div>)
+      showBoard.push(
+        <div key={"row" + ri} className="board-row">
+          {eachRow}
+        </div>
+      );
     }
-
-
 
     return (
       <div>
         <div>Black goes first.</div>
-        <div className="status"><strong>{status}</strong></div>
+        <div className="status">
+          <strong>{status}</strong>
+        </div>
         {showBoard}
-        {/* <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div> */}
-        <button class="btn btn-light" onClick={() => this.undo()}>UNDO</button>
-        <button class="btn btn-light" onClick={() => this.pass()}>PASS</button>
-        <div>Click existing stones to remove them.</div> 
+        <button class="btn btn-light" onClick={() => this.undo()}>
+          UNDO
+        </button>
+        <button class="btn btn-light" onClick={() => this.pass()}>
+          PASS
+        </button>
+        <div>Click existing stones to remove them.</div>
       </div>
     );
   }
 }
-
-export { Board }
+export { Board };
